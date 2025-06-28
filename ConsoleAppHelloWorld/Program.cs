7539a8d56692666226b3cb90311c3802e8bd009a
sharifimdadul25@gmail.com <sharifimdadul25@gmail.com>
2025-06-29 00:37:27 +0530

    Test44

--- ConsoleAppHelloWorld/Program.cs
+++ ConsoleAppHelloWorld/Program.cs
@@ -7,4 +7,5 @@ Console.WriteLine("Hello, World!");
 // 2. Navigate to the directory where this file is located.
 // You can run this application using the .NET CLI or any C# IDE and can not test it directly here.
 // 1. Open a terminal or command prompt in Admin mode.
-// 3. Run the command: dotnet run
+// 3. Run the command: dotnet run 
+// This will compile and execute the application, displaying "Hello, World!" in the console.
