61aa3ef50383285ff30239a1c9715360f0889336
SharifGitRepo <71312043+SharifGitRepo@users.noreply.github.com>
2025-06-28 23:42:34 +0530

    Update Program.cs

--- ConsoleAppHelloWorld/Program.cs
+++ ConsoleAppHelloWorld/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-// You can run this application using the .NET CLI or any C# IDE.
+// You can run this application using the .NET CLI or any C# IDE and can test the application.
 
 
 // 2. Navigate to the directory where this file is located.
