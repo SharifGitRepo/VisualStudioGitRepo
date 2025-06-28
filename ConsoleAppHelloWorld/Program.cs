8f8f57f28bed47b32b79eae0de711d85dda5d79e
sharifimdadul25@gmail.com <sharifimdadul25@gmail.com>
2025-06-28 23:16:36 +0530

    Changes in main

--- ConsoleAppHelloWorld/Program.cs
+++ ConsoleAppHelloWorld/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// This is a simple console application that prints "Hello, World!" to the console.
+// You can run this application using the .NET CLI or any C# IDE.
+// To run this application using the .NET CLI, follow these steps:
+
+// 1. Open a terminal or command prompt.
