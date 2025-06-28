c48e2e63219ee856a9614bdb8062a89466f81b30
sharifimdadul25@gmail.com <sharifimdadul25@gmail.com>
2025-06-28 23:43:44 +0530

    Changes-2

--- ConsoleAppHelloWorld/Program.cs
+++ ConsoleAppHelloWorld/Program.cs
@@ -4,5 +4,11 @@ Console.WriteLine("Hello, World!");
 // You can run this application using the .NET CLI or any C# IDE and can test the application.
 
 // This is a simple console application that prints "Hello, World!" to the console.
+<<<<<<< HEAD
 // 2. Navigate to the directory where this file is located.
+=======
+// You can run this application using the .NET CLI or any C# IDE and can not test it directly here.
+
+
+>>>>>>> 5780245 (Changes-2)
 // 1. Open a terminal or command prompt.
