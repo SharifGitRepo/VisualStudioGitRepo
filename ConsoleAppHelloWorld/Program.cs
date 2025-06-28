44956c5ad2ec59aca96323879d5c3662132d9368
SharifGitRepo <71312043+SharifGitRepo@users.noreply.github.com>
2025-06-28 23:40:01 +0530

    Update Program.cs

--- ConsoleAppHelloWorld/Program.cs
+++ ConsoleAppHelloWorld/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-// This is a simple console application that prints "Hello, World!" to the console.
+
 // You can run this application using the .NET CLI or any C# IDE.
 
 
